334eb09872c1ebda4da9a135ce2e9b25969a5707
Tom Booker <30446949+thomas-booker@users.noreply.github.com>
2021-05-31 21:19:30 +0100

    fixed indentation

--- src/Contact.js
+++ src/Contact.js
@@ -11,39 +11,37 @@ class Contact extends Component {
                         <h2>Contact Me</h2>
                     </div>
                 </div>
-
-                            <div class="row " style={{margin: 5}}>
-                <form class="col s4 blue-grey darken-3 z-depth-2 offset-s4">
-                <div class="row">
-                    <div class="input-field col s6">
-                    <input id="first_name" type="text" class="validate" />
-                    <label for="first_name">First Name</label>
-                    </div>
-                    <div class="input-field col s6">
-                    <input id="last_name" type="text" class="validate" />
-                    <label for="last_name">Last Name</label>
-                    </div>
+                <div class="row " style={{margin: 5}}>
+                    <form class="col s4 blue-grey darken-3 z-depth-2 offset-s4">
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <input id="first_name" type="text" class="validate" />
+                                <label for="first_name">First Name</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <input id="last_name" type="text" class="validate" />
+                                <label for="last_name">Last Name</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <input id="password" type="password" class="validate" />
+                                <label for="password">Your Email Address</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12 ">
+                            <input id="email" type="email" class="validate" />
+                            <label for="email">Message</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s6 offset-s3">
+                                <a class="waves-effect blue-grey darken-2 btn offset-s3" style={{marginBottom: 10}}>Send</a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-                <div class="row">
-                    <div class="input-field col s12">
-                    <input id="password" type="password" class="validate" />
-                    <label for="password">Your Email Address</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12 ">
-                    <input id="email" type="email" class="validate" />
-                    <label for="email">Message</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s6 offset-s3">
-                <a class="waves-effect blue-grey darken-2 btn offset-s3" style={{marginBottom: 10}}>Send</a>
-
-                </div>
-                </div>
-                </form>
-            </div>
             </div>
         )
     }
